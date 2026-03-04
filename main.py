fef22cd78f53ff46ac36cb6178035b58015b8519
stefa <stefaom64@gmail.com>
2026-03-04 23:34:46 +0100

    Create PDF part 1

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 pdf.add_page()
 
+
 pdf.set_font(family="Helvetica", style = "B", size=12)
 pdf.cell(w=26, h=12, text="Hello World", align="L",
          new_x=XPos.LMARGIN, new_y=YPos.NEXT, border=0)
--- main2.py
+++ main2.py
@@ -19,6 +19,7 @@ pdf.set_font(family="Helvetica", size=10)
 pdf.cell(w=0, h=12, txt="Hi There !", align="L",
          ln=1)
 
+
 pdf.add_page()
 
 pdf.set_font(family="Helvetica", style = "B", size=12)
